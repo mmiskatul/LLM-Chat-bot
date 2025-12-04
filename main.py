5404da02fdb9c983b9da84128b2ffd181aa4462c
mmiskatul <masabimiskat@gmail.com>
2025-12-04 10:39:08 +0600

    add the page title and icon and layout and also load the Gooogle_api_key

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import os  
 
+import streamlit as st  
+from dotenv import load_dotenv 
+import google.generativeai as gen_ai 
+
+# load environment variable 
+
+load_dotenv() 
+
+# configuration streamlit page settings 
+
+st.set_page_config(
+    page_title ="My Chat Bot" ,
+    page_icon =":brain:", 
+    layout ="centered"
+)
+
+GOOGLE_API_KEY =os.getenv("GOOGLE_API_KEY") 
+
+# set up google Gemini_pro AI Model 
